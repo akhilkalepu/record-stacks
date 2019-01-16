8dfa678b2d94d55ed2c8260540c489f61fad0527
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-16 18:37:26 -0500

    input form

--- client/src/App.js
+++ client/src/App.js
@@ -14,20 +14,20 @@ class App extends Component {
           <div className="App__Aside"></div>
           <div className="App__Form">
             <div className="PageSwitcher">
-                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-              </div>
+              <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>
 
-              <div className="FormTitle">
-                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-              </div>
+            <div className="FormTitle">
+                <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+            </div>
 
-              <Route exact path="/" component={SignUpForm}>
-              </Route>
-              <Route path="/sign-in" component={SignInForm}>
-              </Route>
-              <Route path="/input" component={InputForm}>
-              </Route>
+            <Route exact path="/" component={SignUpForm}>
+            </Route>
+            <Route path="/sign-in" component={SignInForm}>
+            </Route>
+            <Route path="/input" component={InputForm}>
+            </Route>
           </div>
 
         </div>
--- client/src/pages/InputForm.js
+++ client/src/pages/InputForm.js
@@ -5,19 +5,32 @@ import { Link } from 'react-router-dom';
 class InputForm extends Component {
 
     upload() {
-        var collection = new XMLHttpRequest();
-        collection.open("Get", "collection.nml", false);
-        collection.send();
-        var collectionData = collection.responseText;
-        document.write(collectionData);
-    }
+        // var readXml = null;
+        // $('#xmlForm').submit(function (event) {
+        //     event.preventDefault();
+        //     var selectedFile = document.getElementById('input').files[0];
+        //     console.log(selectedFile);
+        //     var reader = new FileReader();
+        //     reader.onload = function (e) {
+        //         readXml = e.target.result;
+        //         console.log(readXml);
+        //         var parser = new DOMParser();
+        //         var doc = parser.parseFromString(readXml, "application/xml");
+        //         console.log(doc);
+        //     }
+        //     reader.readAsText(selectedFile);
+
+        // });
+    };
     
     render() {
         return (
             <div>
-                <h3> Select a Traktor database file (nml) to upload: </h3>
-                <input id="dbUpload" type="file"></input>
-                <button onclick="upload()">Upload</button>
+                <form  id="xmlForm" name="xmlForm">
+                    <h3> Select a Traktor database file (nml) to upload: </h3>
+                    <input id="dbUpload" type="file"/>
+                    <input type="submit" onClick="upload()"/>
+                </form>
             </div>
         );
     };
