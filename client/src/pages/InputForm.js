0f1ac50f046cefbe77b9a5832731ee159a384915
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-17 15:02:05 -0500

    xml is parsing

--- client/src/pages/InputForm.js
+++ client/src/pages/InputForm.js
@@ -17,10 +17,23 @@ class InputForm extends Component {
             const readerData = evt.target.result;
             const parser = new DOMParser();
             const xml = parser.parseFromString(readerData, 'text/xml');
-            console.log(xml);
+            
             // console.log(xml.querySelector('ENTRY').getAttribute('TITLE'));
-            console.log(xml.getElementsByTagName('ENTRY'));
+            // console.log(xml.getElementsByTagName('ENTRY'));
 
+            console.log(xml);
+            console.log(xml.getElementsByTagName('ENTRY').length);
+            for (var i = 0; i < xml.getElementsByTagName('ENTRY').length; i++) {
+                console.log("----------------------------");
+                console.log("Track: " + xml.getElementsByTagName('ENTRY')[i].getAttribute('TITLE'));
+                console.log("Artist: " + xml.getElementsByTagName('ENTRY')[i].getAttribute('ARTIST'));
+                console.log("Album: " + xml.getElementsByTagName('ENTRY')[i].querySelector('ALBUM').getAttribute("TITLE"));
+                console.log("Release: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("RELEASE_DATE"));
+                console.log("Added: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("IMPORT_DATE"));
+                console.log("Genre: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("GENRE"));
+                console.log("Key: " + xml.getElementsByTagName('ENTRY')[i].querySelector('MUSICAL_KEY').getAttribute("VALUE"));
+                console.log("Tempo: " + xml.getElementsByTagName('ENTRY')[i].querySelector('TEMPO').getAttribute("BPM"));
+            }
         };
     }
     
