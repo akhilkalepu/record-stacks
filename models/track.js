e10eefb307ede7db51c65c7a52b352f88ea0047f
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-17 20:18:03 -0500

    mongo test working

--- models/track.js
+++ models/track.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const trackSchema = new Schema({
-  Track: { type: String, required: true },
+  Track: { type: String, required: false },
   Artist: { type: String, required: false },
   Album: { type: String, required: false },
   Release: { type: Date, required: false },
@@ -11,7 +11,7 @@ const trackSchema = new Schema({
   Tempo: { type: Number, required: false },
   Time: { type: Number, required: false },
   Added: { type: Date, required: false },
-  AUDIO_ID: { type: String, required: true },
+  AUDIO_ID: { type: String, required: false },
 });
 
 module.exports = Track = mongoose.model("item", trackSchema);
--- routes/api/tracks.js
+++ routes/api/tracks.js
@@ -3,10 +3,24 @@ const router = express.Router();
 
 const Track = require("../../models/Track");
 
+
+// @route   GET api/tracks
+// @desc    Get all items
+// @access  Public
 router.get("/", (req, res) => {
     Track.find()
         .sort({ Added: -1})
         .then(tracks => res.json(tracks));
+});
+
+// @route   POST api/tracks
+// @desc    Create a track object
+// @access  Public
+router.post("/", (req, res) => {
+    const newTrack = new Track({
+        Track: req.body.Track
+    })
+    newTrack.save().then(track => res.json(track));
 });
 
 module.exports = router;
