1bc1cee27cc30542301b37d8b4ea62449b865d62
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-18 18:44:13 -0500

    working on store and reducer dummy components

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,35 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import InputForm from './pages/InputForm';
 import AppNavbar from './components/AppNavbar'
+
+
 import List from './components/List';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 class App extends Component {
 	render() {
-			return (
-					<Router basename="/react-auth-ui/">
-							<div className="App">
-									<div>
-											
-											<AppNavbar/>
+		return (
+			<Router basename="/react-auth-ui/">
 
-											<Route exact path="/" component={InputForm}>
-											</Route>
+				<Provider store={store}>
+				<div className="App">
+					<div>
+						<AppNavbar/>
+						<Route exact path="/" component={InputForm}>
+						</Route>
+						<br/>
+						<List/>
+					</div>
+				</div>
+				</Provider>
 
-											<br/>
-
-											<List/>
-
-									</div>
-
-							</div>
-					</Router>
-			);
+			</Router>
+		);
 	}
 }
 
