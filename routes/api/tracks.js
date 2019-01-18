0e91759f39d0cd595ed989a166f181c393ada393
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-17 20:04:56 -0500

    setting up mongodb

--- routes/api/tracks.js
+++ routes/api/tracks.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const Track = require("../../models/Track");
 
-
+router.get("/", (req, res) => {
+    Track.find()
+        .sort({ Added: -1})
+        .then(tracks => res.json(tracks));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,9 +19,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.use("/api/tracks", tracks);
 
