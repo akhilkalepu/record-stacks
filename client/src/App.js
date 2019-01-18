ad6f2b0e6a0434b589a695dd4547256baa12843a
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-18 16:48:38 -0500

    blank canvas with file input

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-import SignUpForm from './pages/SignUpForm';
-import SignInForm from './pages/SignInForm';
 import InputForm from './pages/InputForm';
 
 import './App.css';
@@ -13,21 +11,10 @@ class App extends Component {
         <div className="App">
           <div className="App__Aside"></div>
           <div className="App__Form">
-            <div className="PageSwitcher">
-              <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-            </div>
+            
+            <Route exact path="/" component={InputForm}>
+            </Route>
 
-            <div className="FormTitle">
-                <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-            </div>
-
-            <Route exact path="/" component={SignUpForm}>
-            </Route>
-            <Route exact path="/sign-in" component={SignInForm}>
-            </Route>
-            <Route exact path="/input" component={InputForm}>
-            </Route>
           </div>
 
         </div>
