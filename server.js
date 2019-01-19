837b0c66dfe8dfa0f2c9cbeb2fe483a505028789
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-19 00:01:52 -0500

    heroku edits

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const tracks = require ("./routes/api/tracks");
+const tracks = require("./routes/api/tracks");
 
 const app = express();
 
@@ -13,22 +13,22 @@ app.use(bodyParser.json());
 // DB config
 const db = require("./config/keys").mongoURI;
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-		app.use(express.static("client/build"));
-
-		app.get("*", function (req, res) {
-			res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-		});
-}
-
-app.use("/api/tracks", tracks);
-
 // Connect to the Mongo DB
 mongoose
 	.connect(db)
 	.then(() => console.log("MongoDB connected."))
 	.catch(err => console.log(err));
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+app.use("/api/tracks", tracks);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
