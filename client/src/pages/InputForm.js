0bf610de68e5289b7cd10d50413c8bff3887db9c
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-19 02:45:46 -0500

    full collection sworks with browswer overload alerts

--- client/src/pages/InputForm.js
+++ client/src/pages/InputForm.js
@@ -27,74 +27,57 @@ class InputForm extends Component {
             for (var i = 0; i < xml.getElementsByTagName('ENTRY').length; i++) {
 
                 console.log("----------------------------");
-                console.log("Track: " + xml.getElementsByTagName('ENTRY')[i].getAttribute('TITLE'));
                 var trackVar = xml.getElementsByTagName('ENTRY')[i].getAttribute('TITLE');
 
-                console.log("Artist: " + xml.getElementsByTagName('ENTRY')[i].getAttribute('ARTIST'));
                 var artistVar = xml.getElementsByTagName('ENTRY')[i].getAttribute('ARTIST');
                 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("RELEASE_DATE") == null) {
-                    console.log("Release: null");
                     var releaseVar = null;
                 } else {
-                    console.log("Release: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("RELEASE_DATE"));
                     releaseVar = xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("RELEASE_DATE");
                 }
 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("GENRE") == null) {
-                    console.log("Genre: null");
                     var genreVar = null;
                 } else {
-                    console.log("Genre: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("GENRE"));
                     genreVar = xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("GENRE");
                 }
 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('MUSICAL_KEY') == null) {
-                    console.log("Key: null");
                     var keyVar = null;
                 } else {
                     if (xml.getElementsByTagName('ENTRY')[i].querySelector('MUSICAL_KEY').getAttribute("VALUE") == null) {
-                        console.log("Key: null");
                         keyVar = null;
                     } else {
-                        console.log("Key: " + xml.getElementsByTagName('ENTRY')[i].querySelector('MUSICAL_KEY').getAttribute("VALUE"));
                         keyVar = xml.getElementsByTagName('ENTRY')[i].querySelector('MUSICAL_KEY').getAttribute("VALUE");
                     }
                 }
                 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('TEMPO') == null) {
-                    console.log("Tempo: null");
                     var tempoVar = null;
                 } else {
                     if (xml.getElementsByTagName('ENTRY')[i].querySelector('TEMPO').getAttribute("BPM") == null) {
-                        console.log("Tempo: null");
                         tempoVar = null;
                     } else {
-                        console.log("Tempo: " + xml.getElementsByTagName('ENTRY')[i].querySelector('TEMPO').getAttribute("BPM"));
                         tempoVar = xml.getElementsByTagName('ENTRY')[i].querySelector('TEMPO').getAttribute("BPM");
                     }
                 }
 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute('PLAYTIME') == null) {
-                    console.log("Time: null");
                     var timeVar = null;
                 } else {
-                    console.log("Time: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute('PLAYTIME'));
                     timeVar = xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute('PLAYTIME');
                 }
 
                 if (xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("IMPORT_DATE") == null) {
-                    console.log("Added: null");
                     var addedVar = null;
                 } else {
-                    console.log("Added: " + xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("IMPORT_DATE"));
                     addedVar = xml.getElementsByTagName('ENTRY')[i].querySelector('INFO').getAttribute("IMPORT_DATE");
                 }
                 
-                console.log("AUDIO_ID: " + xml.getElementsByTagName('ENTRY')[i].getAttribute('AUDIO_ID'));
                 var audioidVar = xml.getElementsByTagName('ENTRY')[i].getAttribute('AUDIO_ID');
 
-                console.log("Track Number: " + (i + 1));
+                console.log("Track Number: " + (i + 1) + "sent to MongoDB");
 
                 const newItem = {
                     Track: trackVar,
