927186b5097ca16c789598a9a5e3af1fdcb45f43
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-20 18:18:22 -0500

    tempo bar working

--- client/src/pages/GraphPage.js
+++ client/src/pages/GraphPage.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import {
   Container
 } from 'reactstrap';
-import {Pie, HorizontalBar} from 'react-chartjs-2';
+import {
+  Pie,
+  Bar,
+  HorizontalBar
+} from 'react-chartjs-2';
 import axios from 'axios';
 // import { connect } from 'react-redux';
 // import { getItems } from '../actions/itemActions';
@@ -26,11 +30,14 @@ class GenrePieChart extends Component {
         const collection = res.data;
 
         let KeyArray = [];
+        let Tempo10Array = [];
+        let TempoArray = [];
         let GenreArray = [];
 
         collection.forEach(element => {
 
           KeyArray.push(element.Key);
+          TempoArray.push(element.Tempo);
           GenreArray.push(element.Genre);
           
         });
@@ -41,25 +48,58 @@ class GenrePieChart extends Component {
         }
 
         const keys = KeyArray.filter(unique).sort(function(a, b){return a - b});
+        
+        for (var i = 0; i < TempoArray.length; i++) {
+          Tempo10Array[i] = Math.round(TempoArray[i] / 10) * 10;
+        }
+        var tempos10 = Tempo10Array.filter(unique).sort(function (a, b) {return a - b});
+
+        for (i = 0; i < TempoArray.length; i++) {
+          TempoArray[i] = Math.round(TempoArray[i]);
+        }
+        var tempos = TempoArray.filter(unique).sort(function(a, b){return a - b});
+
         const genres = GenreArray.filter(unique).sort();
 
         // Get data counts
         let keysCount = [];
-        for (var k = 0; k < keys.length; k++) {
+        for (i = 0; i < keys.length; i++) {
           var count = 0;
           for (var j = 0; j < KeyArray.length; j++) {
-            if (keys[k] === KeyArray[j]) {
+            if (keys[i] === KeyArray[j]) {
               count++;
             }
           }
           keysCount.push(count);
         }
 
+        let tempos10Count = [];
+        for (i = 0; i < tempos10.length; i++) {
+          count = 0;
+          for (j = 0; j < Tempo10Array.length; j++) {
+            if (tempos10[i] === Tempo10Array[j]) {
+              count++;
+            }
+          }
+          tempos10Count.push(count);
+        }
+
+        let temposCount = [];
+        for (i = 0; i < tempos.length; i++) {
+          count = 0;
+          for (j = 0; j < TempoArray.length; j++) {
+            if (tempos[i] === TempoArray[j]) {
+              count++;
+            }
+          }
+          temposCount.push(count);
+        }
+
         let genresCount = [];
-        for (var k = 0; k < genres.length; k++) {
-          var count = 0;
-          for (var j = 0; j < GenreArray.length; j++) {
-            if (genres[k] === GenreArray[j]) {
+        for (i = 0; i < genres.length; i++) {
+          count = 0;
+          for (j = 0; j < GenreArray.length; j++) {
+            if (genres[i] === GenreArray[j]) {
               count++;
             }
           }
@@ -68,6 +108,7 @@ class GenrePieChart extends Component {
         
         // Get random colors for chart
         var keysColors = [];
+        var tempos10Colors = [];
         var genresColors = [];
 
         var dynamicColors = function () {
@@ -77,10 +118,13 @@ class GenrePieChart extends Component {
           return "rgb(" + r + "," + g + "," + b + ")";
         };
         
-        for (var i = 0; i < keys.length; i++) {
+        for (i = 0; i < keys.length; i++) {
           keysColors.push(dynamicColors());
         }
-        for (var i = 0; i < genres.length; i++) {
+        for (i = 0; i < tempos.length; i++) {
+          tempos10Colors.push(dynamicColors());
+        }
+        for (i = 0; i < genres.length; i++) {
           genresColors.push(dynamicColors());
         }
 
@@ -92,6 +136,15 @@ class GenrePieChart extends Component {
               label: 'Key Bar Chart',
               data: keysCount,
               backgroundColor: keysColors
+            }]
+          },
+
+          TempoBar: {
+            labels: tempos10,
+            datasets: [{
+              label: 'Tempo Bar Chart',
+              data: tempos10Count,
+              backgroundColor: tempos10Colors
             }]
           },
 
@@ -116,7 +169,19 @@ class GenrePieChart extends Component {
           <h2>Keys</h2>
           <HorizontalBar
             data={this.state.KeyBar}
-            height="1000"
+            height="750"
+            options={{
+              maintainAspectRatio: false
+            }}/>
+        </div>
+        
+        <br/>
+
+        <div>
+          <h2>Tempos</h2>
+          <Bar
+            data={this.state.TempoBar}
+            height="500"
             options={{
               maintainAspectRatio: false
             }}/>
