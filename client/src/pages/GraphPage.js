8025d105d9f8140ac656d194dfe360eef79aa836
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-19 20:30:54 -0500

    chart.js example working with mongo

--- client/src/pages/GraphPage.js
+++ client/src/pages/GraphPage.js
@@ -3,16 +3,70 @@ import {
   Container,
   Button
 } from 'reactstrap';
-
-import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
-import PropTypes from 'prop-types';
+import {Pie} from 'react-chartjs-2';
+import axios from 'axios';
+// import { connect } from 'react-redux';
+// import { getItems } from '../actions/itemActions';
+// import PropTypes from 'prop-types';
 
 class GraphPage extends Component {
     
-    getTracks() {
-      this.props.getItems();
+    constructor(props) {
+      super(props);
+      this.state = {
+        Data: {}
+      }
+    }
+
+    componentDidMount() {
       console.log("getting items")
+      axios.get(`/api/tracks`)
+        .then(res => {
+          const collection = res.data;
+          let TrackArray = [];
+          let ArtistArray = [];
+          let ReleaseArray = [];
+          let GenreArray = [];
+          let KeyArray = [];
+          let TempoArray = [];
+          let TimeArray = [];
+          let AddedArray = [];
+          let AUDIO_IDArray = [];
+          collection.forEach(element => {
+            TrackArray.push(element.Track);
+            ArtistArray.push(element.Artist);
+            ReleaseArray.push(element.Release);
+            GenreArray.push(element.Genre);
+            KeyArray.push(element.Key);
+            TempoArray.push(element.Tempo);
+            TimeArray.push(element.Time);
+            AddedArray.push(element.Added);
+            AUDIO_IDArray.push(element.AUDIO_ID);
+          });
+          this.setState({
+            Data: {
+              labels: TrackArray,
+              datasets: [{
+                label: 'Track Time',
+                data: TimeArray,
+                backgroundColor: [
+                  'rgba(255,105,145,0.6)',
+                  'rgba(155,100,210,0.6)',
+                  'rgba(90,178,255,0.6)',
+                  'rgba(240,134,67,0.6)',
+                  'rgba(120,120,120,0.6)',
+                  'rgba(250,55,197,0.6)',
+                  'rgba(255,105,145,0.6)',
+                  'rgba(155,100,210,0.6)',
+                  'rgba(90,178,255,0.6)',
+                  'rgba(240,134,67,0.6)',
+                  'rgba(120,120,120,0.6)',
+                  'rgba(250,55,197,0.6)'
+                ]
+              }]
+            }
+          });
+        })
     }
 
     visualizeTracks() {
@@ -25,7 +79,7 @@ class GraphPage extends Component {
         
         <Container>
           
-          <Button
+          {/* <Button
             color="dark"
             style={{margin: "2rem"}}
             onClick={this.getTracks}
@@ -35,20 +89,17 @@ class GraphPage extends Component {
             color="secondary"
             style={{margin: "2rem"}}
             onClick={this.visualizeData}
-          >Visualize</Button>
+          >Visualize</Button> */}
+
+          <div>
+            <Pie
+              data={this.state.Data}
+              options={{maintainAspectRatio: true}}/>
+          </div>
 
         </Container>
       );
     }
 }
 
-GraphPage.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => ({
-  item: state.item
-});
-
-export default connect(mapStateToProps, { getItems })(GraphPage);
+export default GraphPage;
