2f2256b0d38e23199218a1417387732bbb2fe9ba
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-21 14:24:41 -0500

    working on okta authorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 
-import { HashRouter as Router, Route } from 'react-router-dom';
 import Login from "./components/auth/Login"
 import InputForm from './pages/InputForm';
 import AppNavbar from './components/AppNavbar'
@@ -12,29 +13,42 @@ import store from './store';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+function onAuthRequired({history}) {
+  history.push("/login");
+}
+
 class App extends Component {
 
 	render() {
 
 		return (
-			<Router basename="/react-auth-ui/">
-				<Provider store={store}>
-					<div className="App">
-						<div>
+			<Router>
+        <Security issuer="https://dev-406703.oktapreview.com/oauth2/default"
+        client_id="0oaj1tgz24gpMjG5R0h7"
+        redirect_uri={window.location.origin + "/implicit/callback"}
+        onAuthRequired={onAuthRequired}>
 
-							<AppNavbar />
+          <Provider store={store}>
+            <div className="App">
+              <div>
 
-              <div className="container">
+                <AppNavbar />
 
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/" component={InputForm} />
-                <Route exact path="/" component={GraphPage} />
+                <div className="container">
+
+                  <Route path="/login" exact={true} render={(InputForm) => <Login baseUrl="https://dev-406703.oktapreview.com"/>} />
+                  <Route path="/implicit/callback" exact={true} component={ImplicitCallback} />
+
+                  <SecureRoute exact path="/" component={InputForm} />
+                  <SecureRoute exact path="/" component={GraphPage} />
+
+                </div>
 
               </div>
+            </div>
+          </Provider>
 
-						</div>
-					</div>
-				</Provider>
+        </Security>
 			</Router>
 		);
 	}
