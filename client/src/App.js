a51d592468e758c7fab05e93a44955c74ea96892
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-30 17:31:53 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import axios from 'axios'
 
 import AppNavbar from './components/AppNavbar';
 import Home from "./pages/Home";
